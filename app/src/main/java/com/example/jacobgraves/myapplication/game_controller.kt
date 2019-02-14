bfa7c4cb49b76a526c6f71a2a3c450e256bb6d5c
BigBrownAfro <bigbrownafro@gmail.com>
2019-02-13 21:51:46 -0700

    Merge branch 'Develop' of https://bitbucket.org/rushbnostop/project into Develop
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	.idea/misc.xml
    #	app/src/main/res/layout/main_menu_view.xml

---
+++ app/src/main/java/com/example/jacobgraves/myapplication/game_controller.kt
@@ -0,0 +1,13 @@
+package com.example.jacobgraves.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.character_select_view.*
+
+class game_controller : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game_view)
+
+    }
+}
--- app/src/main/java/com/example/jacobgraves/myapplication/main_menu_controller.kt
+++ app/src/main/java/com/example/jacobgraves/myapplication/main_menu_controller.kt
@@ -1,4 +1,45 @@
 package com.example.jacobgraves.myapplication
 
-class main_menu_controller {
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.character_select_view.*
+import kotlinx.android.synthetic.main.main_menu_view.*
+
+class main_menu_controller : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_menu_view)
+
+        configure_buttons()
+
+    }
+
+    fun configure_buttons(){
+        play_button.setOnClickListener {
+            play_button_clicked()
+        }
+        shop_button.setOnClickListener {
+            shop_button_clicked()
+        }
+        settings_button.setOnClickListener {
+            settings_button_clicked()
+        }
+    }
+
+    fun play_button_clicked(){
+        val intent = Intent(this, character_select_controller::class.java)
+        this.startActivity(intent)
+    }
+
+    fun shop_button_clicked(){
+        val intent = Intent(this, shop_controller::class.java)
+        this.startActivity(intent)
+    }
+
+    fun settings_button_clicked(){
+        val intent = Intent(this, settings_controller::class.java)
+        this.startActivity(intent)
+    }
 }
