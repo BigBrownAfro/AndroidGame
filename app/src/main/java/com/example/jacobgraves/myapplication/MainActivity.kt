31c4e63c3a92a792a887c0c14014808cb8817b1f
BigBrownAfro <bigbrownafro@gmail.com>
2019-02-13 19:53:50 -0700

    Created views and sample buttons for the main menu, character select, and game view with buttons that segue between them.

--- app/src/main/java/com/example/jacobgraves/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/jacobgraves/myapplication/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.jacobgraves.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.character_select_view.*
+import kotlinx.android.synthetic.main.main_menu_view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_menu_view)
+
+        play_button.setOnClickListener {
+            play_button.text = "Got Me!"
+            setContentView(R.layout.character_select_view)
+            character_select_button.setOnClickListener {
+                setContentView(R.layout.game_view)
+            }
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/jacobgraves/myapplication/main_menu_controller.kt
@@ -0,0 +1,4 @@
+package com.example.jacobgraves.myapplication
+
+class main_menu_controller {
+}
