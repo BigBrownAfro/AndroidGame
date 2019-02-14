f64505b3231afca53a861a1b09075300223f5ccd
Bluehagrid <jacob.jackson@dixiesuccess.org>
2019-02-13 22:02:27 -0700

    Building GUI. Tested the changing of images to update health bar.

--- app/src/main/java/com/example/jacobgraves/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/jacobgraves/myapplication/MainActivity.kt
@@ -2,24 +2,28 @@ package com.example.jacobgraves.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import kotlinx.android.synthetic.main.character_select_view.*
+import kotlinx.android.synthetic.main.game_view.*
 import kotlinx.android.synthetic.main.main_menu_view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_menu_view)
+        setContentView(R.layout.game_view)
 
-        play_button.setOnClickListener {
-            play_button.text = "Got Me!"
-            setContentView(R.layout.character_select_view)
-            character_select_button.setOnClickListener {
-                setContentView(R.layout.game_view)
+        damage_button.setOnClickListener {
+            health_image_view.setImageResource(R.drawable.test_heart)
+
+
+
+
             }
         }
 
 
 
     }
-}
+
